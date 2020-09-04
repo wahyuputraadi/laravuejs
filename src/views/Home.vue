f036e2a2ae67861a30ec2b1202ef69873804752c
wahyuputraadi <wahyuadii516@gmail.com>
2020-09-04 15:07:55 +0700

    Commit Pertama

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <header-shayna />
+    <banner-hero />
+    <banner-begin/>
+    <banner-insta/>
+    <logo-partners/>
+    <footer-shayna/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import BannerHero from "@/components/BannerHero.vue";
+import BannerBegin from "@/components/BannerBegin.vue";
+import BannerInsta from "@/components/BannerInsta.vue";
+import LogoPartners from "@/components/LogoPartners.vue";
+import FooterShayna from '@/components/FooterShayna';
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+    HeaderShayna,
+    BannerHero,
+    BannerBegin,
+    BannerInsta,
+    LogoPartners,
+    FooterShayna,
+  },
+};
+</script>
